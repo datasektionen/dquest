3d7858f1d4a44bf00576877e8d79eac043e0cc18
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2017-10-26 21:01:35 +0200

    Made it compile...

--- server/src/Servant/Ext.hs
+++ server/src/Servant/Ext.hs
@@ -9,8 +9,11 @@ import Network.HTTP.Media ((//))
 import Data.ByteString.Lazy as BL
 import System.Directory
 import Servant.Ext.Types as Types
+import Servant.Server.Internal.Handler
+import Servant.Server.Internal.ServantErr (err404)
 
 import Control.Monad.Trans
+import Control.Monad.Except
 import Control.Monad
 
 
